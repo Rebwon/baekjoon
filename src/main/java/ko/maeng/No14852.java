7b6cafd0ef03cfbeb309c999a7dfec0a16599cfd
Rebwon <msolo021015@naver.com>
2019-03-21 19:13:30 +0900

    2019/03/21 No14852 Solving

--- src/main/java/ko/maeng/No14852.java
+++ src/main/java/ko/maeng/No14852.java
@@ -5,9 +5,21 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class No14852 {
+    static final int MOD = (int)(1e+9)+7;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        System.out.println();
+
+        long dp[] = new long[n+10];
+        long dp2[] = new long[n+10];
+        dp[1] = 2;
+        dp[2] = 7;
+        dp[3] = 22;
+        dp2[3] = 1;
+        for(int i=4; i<=n; i++){
+            dp2[i] = (dp2[i-1]+dp[i-3])%MOD;
+            dp[i] = (dp[i-2]*3+dp[i-1]*2+dp2[i]*2)%MOD;
+        }
+        System.out.println(dp[n]);
     }
 }
